0ee5ae8fd40f8e1a64e73f747da22d55b268ef93
Daniel Luengo <danielluengoecheverria@gmail.com>
2023-03-23 20:27:27 +0100

    Api Curso

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::controller(ProductoController::class)->prefix('product')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+    Route::get('/{id}', 'show');
+    Route::post('/{id}', 'update');
+    Route::put('/{id}', 'put');
+    Route::delete('/{id}', 'destroy');
+});
+
+Route::controller(CursoController::class)->prefix('curso')->group(function () {
     Route::get('/', 'index');
     Route::post('/', 'store');
     Route::get('/{id}', 'show');
